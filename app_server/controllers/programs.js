f30dabcebaafecd84dca2eb60fc77f61af47d28a
Sojung Choi <sojc31@gmail.com>
2020-10-02 20:03:38 -0700

    created mongodb connection

--- app_server/controllers/programs.js
+++ app_server/controllers/programs.js
@@ -1,6 +1,33 @@
 /* GET 'home' page */
 const homelist = (req, res) => {
-  res.render('programs-list', {title: 'Home'});
+  res.render('programs-list', {
+    title: 'Washington State Housing Finance Commission',
+    pageHeader: {
+        title: 'WSHFC',
+        strapline: 'Opening doors to a better life'
+    },
+
+    sidebar: 'Making affordable homes and healthy communities a reality for everyone.',
+    programs: [{
+        name: '9% Housing Credit',
+        address: '1000 Second Ave, Seattle, WA 98122',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '100m'
+    },{
+        name: '4% Housing Credit',
+        address: '1000 Second Ave, Seattle, WA 98122',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '100m'
+    },{
+        name: 'Nonprofit Housing',
+        address: '1000 Second Ave, Seattle, WA 98122',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '100m'
+    }]
+  });
 };
 
 /* GET 'program info' page */
